b00a65c87c4d65dae068118d3737b00ba524ff58
Parth Master <parthmaster5@gmail.com>
2024-03-24 23:17:25 +1100

    Segregated the logic for app listen and app configuration and added more test cases

--- src/routes/urlShortener.test.ts
+++ src/routes/urlShortener.test.ts
@@ -1,7 +1,7 @@
 
 import UrlMapping from '../models/urlMappingModel';
 const request = require('supertest');
-const app = require('../index').default;
+const app = require('../app').default;
 
 describe('UrlMapping', () => {
   test('should create a new UrlMapping object with the provided originalUrl and shortUrl', () => {
@@ -19,7 +19,17 @@ describe('UrlMapping', () => {
 });
 
 
+// for the post request we will consider three scenarios , valid case , invalid case and edge case where the original url is not unique
 describe('URL Shortener API', () => {
+    it('should return status 200 for valid URL', async () => {
+        const response = await request(app)
+            .post('/shorten/long_url')
+            .send({ originalUrl: 'https://example.com' });
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('shortUrl');
+    });
+
     it('should return status 400 for invalid URL', async () => {
         const response = await request(app)
             .post('/shorten/long_url')
@@ -28,7 +38,14 @@ describe('URL Shortener API', () => {
         expect(response.status).toBe(400);
         expect(response.body).not.toHaveProperty('shortUrl');
     });
+
+    it('should return status 400 for duplicate URL', async () => {
+        const response = await request(app)
+            .post('/shorten/long_url')
+            .send({ originalUrl: 'https://example.com' });
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('shortUrl');
+    });
 });
-
-
 
